8ec93a664f976a0124ec0202ee5821351cf701ff
Robie Basak <robie.basak@oss.qualcomm.com>
2025-11-14 14:28:01 +0000

    ci: add and integrate qemu_guest_test.py
    This allows us to write tests that run natively on the guest, and
    qemu_test.py will invoke them using qemu.
    
    Signed-off-by: Robie Basak <robie.basak@oss.qualcomm.com>

--- ci/qemu_test.py
+++ ci/qemu_test.py
@@ -13,6 +13,12 @@ import types
 import pexpect
 import pytest
 
+# Since the first test checks for the mandatory password reset functionality
+# that also prepares the VM for shell-based access, we make the additional
+# optimisation that the fixture for a logged in VM re-uses that VM, so the
+# ordering of [plain VM fixture, password reset test, logged-in VM fixture]
+# matters here.
+
 
 @pytest.fixture(scope="module")
 def vm():
@@ -58,10 +64,14 @@ def vm():
                 "-nographic",
                 "-bios",
                 "/usr/share/AAVMF/AAVMF_CODE.fd",
+                "-fsdev",
+                f"local,id=fsdev0,path={os.getcwd()},security_model=none",
+                "-device",
+                "virtio-9p-pci,fsdev=fsdev0,mount_tag=qcom-deb-images",
             ],
         )
         spawn.logfile = sys.stdout.buffer
-        yield types.SimpleNamespace(spawn=spawn)
+        yield types.SimpleNamespace(spawn=spawn, logged_in=False)
 
         # No need to be nice; that would take time
         spawn.kill(signal.SIGKILL)
@@ -89,3 +99,42 @@ def test_password_reset_required(vm):
     vm.spawn.expect_exact("Retype new password:")
     vm.spawn.send("new password\r\n")
     vm.spawn.expect_exact("debian@debian:~$")
+
+    vm.logged_in = True
+
+
+@pytest.fixture(scope="module")
+def logged_in_vm(vm):
+    if not vm.logged_in:
+        pytest.skip("Password reset test did not run or failed")
+    return vm
+
+
+def test_using_guest_tests(logged_in_vm):
+    """Run the tests in qemu_guest_test.py inside the qemu guest"""
+    # Statement of test success and failure that are unlikely to appear by
+    # accident
+    SUCCESS_NOTICE = "All ci/qemu_guest_test.py tests passed"
+    FAILURE_NOTICE = "Some ci/qemu_guest_test.py tests failed"
+    # We use apt-get -U here and the apt_dependencies fixture in
+    # qemu_guest_test.py relies on this.
+    SCRIPT = f"""sudo -i sh <<EOT
+apt-get install -Uy --no-install-recommends python3-pytest
+mkdir qcom-deb-images
+mount -t 9p qcom-deb-images qcom-deb-images
+cd qcom-deb-images
+py.test-3 -vvm guest ci/qemu_guest_test.py && echo "{SUCCESS_NOTICE}" || echo "{FAILURE_NOTICE}"
+EOT
+"""
+    logged_in_vm.spawn.send(SCRIPT.replace("\r", "\r\n"))
+
+    # Match a known string for when pytest starts. Otherwise we catch the echo
+    # of our own printing of SUCCESS_NOTICE and FAILURE_NOTICE that appears
+    # before, causing us to falsely believe that it was done. The timeout is
+    # required to give enough time for the installation of python3-pytest to
+    # finish.
+    logged_in_vm.spawn.expect_exact("test session starts", timeout=120)
+    match = logged_in_vm.spawn.expect_exact(
+        [SUCCESS_NOTICE, FAILURE_NOTICE], timeout=120
+    )
+    assert match == 0, "ci/qemu_guest_test.py tests failed"
