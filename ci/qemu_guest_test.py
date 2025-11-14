d8a43b6367f055f17d944fc587be54fb5ed60c08
Robie Basak <robie.basak@oss.qualcomm.com>
2025-11-14 14:31:02 +0000

    qemu_guest_test: add test for sudo fqdn issue
    If DNS is misconfigured in the deployment environment, sudo may hang
    while it times out on DNS requests, which is not desirable for the
    majority of our users. Add a test that identifies if sudo is using DNS.
    
    See: https://github.com/qualcomm-linux/qcom-deb-images/issues/193
    
    There isn't an easy and obvious method of testing "did sudo use DNS"
    that (ideally) is independent of the DNS configuration in the test
    environment. This is hopefully unintrusive and reasonably flexible.
    
    The catch is that if we use sudo-rs in the future then it might stop
    working if Rust doesn't use libc. But this seems better than nothing.
    
    To make it easier to ensure that this test is correctly testing what we
    need, I've separated the test from the fix using xfail. This way, before
    the issue is fixed, the test suite passing means that the test is
    correctly detecting the issue. I'll then introduce the fix in the next
    change.
    
    Signed-off-by: Robie Basak <robie.basak@oss.qualcomm.com>

--- ci/qemu_guest_test.py
+++ ci/qemu_guest_test.py
@@ -6,6 +6,9 @@
 # These tests are run inside the qemu guest as root using its own pytest runner
 # invocation.
 
+import subprocess
+import tempfile
+
 import pytest
 
 # Mark this module so that the main test runner can skip it when running from
@@ -20,3 +23,69 @@ def test_empty():
     # The empty test. This is nevertheless useful as its presence ensures that
     # the host is calling the guest test suite in this module correctly.
     pass
+
+
+# To keep tests fast for developer iteration, just install all the test
+# dependencies together once for all tests defined here. It is likely that our
+# tests here are not going to interact. If they do, then we can decide whether
+# to compromise on this at that time.
+@pytest.fixture(scope="module")
+def apt_dependencies():
+    # To speed things up, we deliberately skip the apt-get update here on the
+    # assumption that it was arranged by whatever is running the test. This is
+    # arranged from qemu_test.py::test_using_guest_tests() instead.
+    subprocess.run(
+        ["apt-get", "install", "-y", "--no-install-recommends", "sudo", "gdb"],
+        check=True,
+    )
+
+
+@pytest.mark.xfail(
+    reason="https://github.com/qualcomm-linux/qcom-deb-images/issues/193",
+    strict=True,
+)
+def test_sudo_no_fqdn(apt_dependencies):
+    """sudo should not call FQDN lookup functions
+
+    See: https://github.com/qualcomm-linux/qcom-deb-images/issues/193
+    """
+    with tempfile.NamedTemporaryFile(
+        mode="w", delete_on_close=False
+    ) as gdb_commands_file:
+        print(
+            "catch load",
+            "run",
+            "del 1",
+            sep="\n",
+            file=gdb_commands_file,
+        )
+        for fn_name in [
+            "gethostbyaddr",
+            "getnameinfo",
+            "getaddrinfo",
+            "gethostbyname",
+        ]:
+            print(
+                f"break {fn_name}",
+                f"commands",
+                f'    print "{fn_name} called\\n"',
+                f"    quit 1",
+                f"end",
+                sep="\n",
+                file=gdb_commands_file,
+            )
+        print("continue", file=gdb_commands_file)
+        gdb_commands_file.close()
+
+        subprocess.run(
+            [
+                "gdb",
+                "--batch",
+                "-x",
+                gdb_commands_file.name,
+                "--args",
+                "sudo",
+                "true",
+            ],
+            check=True,
+        )
